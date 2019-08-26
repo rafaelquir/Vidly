b07ea8fcc2b8508bf55c2be5c2846663f1d578cf
Rafael Quiroz <rafael_quirozve@hotmail.com>
2019-08-26 23:44:56 +0200

    Agrego Base de datos por Entity Framework

---
+++ Vidly/Models/IdentityModels.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity;
+using System.Dynamic;
+using System.Security.AccessControl;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Vidly.Models
+{
+    // Para agregar datos de perfil del usuario, agregue más propiedades a su clase ApplicationUser. Visite https://go.microsoft.com/fwlink/?LinkID=317594 para obtener más información.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Tenga en cuenta que el valor de authenticationType debe coincidir con el definido en CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Agregar aquí notificaciones personalizadas de usuario
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public System.Data.Entity.DbSet<Vidly.Models.Customer> Customers { get; set; }
+
+        public System.Data.Entity.DbSet<Vidly.Models.Movie> Movies { get; set; }
+    }
+}
