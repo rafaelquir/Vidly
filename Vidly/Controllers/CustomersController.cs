16f682580ed75874e83b85c309a422dff05b46db
Rafael Quiroz <rafael_quirozve@hotmail.com>
2019-08-28 16:46:59 +0200

    Agregando el formulario para editar al cliente

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -23,6 +23,21 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                TryUpdateModel(customerInDb);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
 
         // GET: Customers
         public ViewResult Index()
@@ -35,11 +50,11 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         public ActionResult Details(int id)
@@ -50,6 +65,21 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes =  _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
