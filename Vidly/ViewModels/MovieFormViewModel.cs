5f31dc9804be6bd35ccf0b779021633b0b48767c
Rafael Quiroz <rafael_quirozve@hotmail.com>
2019-10-12 20:07:37 +0200

    agregando Mappers

--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using Vidly.Models;
@@ -11,16 +12,45 @@ namespace Vidly.ViewModels
         public IEnumerable<Genre> Genres { get; set; }
         public Movie Movie { get; set; }
 
+        public int? Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Display(Name = "Genero")]
+        [Required]
+        public byte? GenreId { get; set; }
+
+        [Display(Name = "Día de entrega")]
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Display(Name = "Cantidad en existencia")]
+        [Range(1, 20)]
+        [Required]
+        public byte? NumberInStock { get; set; }
+
         public string Title
         {
             get
             {
-                if (Movie != null && Movie.Id != 0)
-                    return "Edit Movie";
-
-                return "New Movie";
+                return Id != 0 ? "Editar Pelicula" : "Nueva Pelicula";
             }
         }
 
+        public MovieFormViewModel()
+        {
+            Id = 0;
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleaseDate = movie.ReleaseDate;
+            NumberInStock = movie.NumberInStock;
+            GenreId = movie.GenreId;
+        }
     }
 }
